209530019de8bd7943940e5e75319d8119de9047
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-07-17 14:32:51 +0200

    (comments)

--- src/lib/style/layout_style.rs
+++ src/lib/style/layout_style.rs
@@ -4,10 +4,9 @@
 pub use stretch::geometry::Rect;
 /// Re-export from [`stretch::geometry::Size`](https://docs.rs/stretch/latest/stretch/geometry/struct.Size.html)
 pub use stretch::geometry::Size;
-/// Re-export from
-/// [`stretch::number::Number`](https://docs.rs/stretch/latest/stretch/number/enum.Number.html)
-pub use stretch::number::Number::{self, Defined}; // Undefined clashes with
-                                                  // Dimension::Undefined
+/// Re-export from [`stretch::number::Number`](https://docs.rs/stretch/latest/stretch/number/enum.Number.html)
+// Undefined clashes with Dimension::Undefined
+pub use stretch::number::Number::{self, Defined};
 /// Re-export from [`stretch::style::AlignContent`](https://docs.rs/stretch/latest/stretch/style/enum.AlignContent.html)
 pub use stretch::style::AlignContent::{self, *};
 /// Re-export from [`stretch::style::AlignItems`](https://docs.rs/stretch/latest/stretch/style/enum.AlignItems.html)
@@ -19,7 +18,8 @@ pub use stretch::style::Dimension::{self, *};
 /// Re-export from [`stretch::style::Direction`](https://docs.rs/stretch/latest/stretch/style/enum.Direction.html)
 pub use stretch::style::Direction::{self, *};
 /// Re-export from [`stretch::style::Display`](https://docs.rs/stretch/latest/stretch/style/enum.Display.html)
-pub use stretch::style::Display::{self, Flex}; // None clashes with Option::None
+// None clashes with Option::None
+pub use stretch::style::Display::{self, Flex};
 /// Re-export from [`stretch::style::FlexDirection`](https://docs.rs/stretch/latest/stretch/style/enum.FlexDirection.html)
 pub use stretch::style::FlexDirection::{self, *};
 /// Re-export from [`stretch::style::FlexWrap`](https://docs.rs/stretch/latest/stretch/style/enum.FlexWrap.html)
