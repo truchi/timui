9bc5ed1052388d080212c1f4ae9a9668832f4f9a
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-07-18 01:20:39 +0200

    Deleted ColorStyleInherit

--- src/lib/style/mod.rs
+++ src/lib/style/mod.rs
@@ -6,12 +6,10 @@
 
 mod color;
 mod color_style;
-// mod color_style_inherit;
 mod layout_style;
 mod style;
 
 pub use color::*;
 pub use color_style::*;
-// pub use color_style_inherit::*;
 pub use layout_style::*;
 pub use style::*;
