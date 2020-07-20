52f7a95f34606defb19791385c042db9912c3073
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-07-20 08:20:45 +0200

    Uniform

--- src/lib/paint/mod.rs
+++ src/lib/paint/mod.rs
@@ -7,11 +7,11 @@ mod cell;
 mod layer;
 mod rect;
 // mod paint;
-// mod uniform;
+mod uniform;
 
 // pub use canvas::*;
 pub use cell::*;
 pub use layer::*;
 pub use rect::*;
 // pub use paint::*;
-// pub use uniform::*;
+pub use uniform::*;
--- src/lib/paint/uniform.rs
+++ src/lib/paint/uniform.rs
@@ -1,50 +1,30 @@
-use super::{Cell, Layer};
-use crate::style::ColorStyle;
-use stretch::result::Layout;
+use super::{Cell, Layer, Rect};
 
+/// A uniform `Layer`: same same all over the layer
 #[derive(Default, Debug)]
 pub struct Uniform {
-    pub x:      usize,
-    pub y:      usize,
-    pub width:  usize,
-    pub height: usize,
-    pub cell:   Cell,
+    /// The `Rect` bounds of the `Layer`
+    rect: Rect,
+    /// The `Cell` filling the `Layer`
+    cell: Cell,
+}
+
+impl Uniform {
+    /// Creates a `Uniform`
+    pub fn new(rect: impl Into<Rect>, cell: Cell) -> Self {
+        Self {
+            rect: rect.into(),
+            cell,
+        }
+    }
 }
 
 impl Layer for Uniform {
-    fn x(&self) -> usize {
-        self.x
-    }
-
-    fn y(&self) -> usize {
-        self.y
-    }
-
-    fn width(&self) -> usize {
-        self.width
-    }
-
-    fn height(&self) -> usize {
-        self.height
-    }
-
-    fn get(&self, _: usize, _: usize) -> &Cell {
+    fn get(&self, _: u16, _: u16) -> &Cell {
         &self.cell
     }
 
-    fn get_mut(&mut self, _: usize, _: usize) -> &mut Cell {
+    fn get_mut(&mut self, _: u16, _: u16) -> &mut Cell {
         &mut self.cell
-    }
-}
-
-impl From<(Layout, ColorStyle, char)> for Uniform {
-    fn from((layout, style, c): (Layout, ColorStyle, char)) -> Self {
-        Self {
-            x:      layout.location.x as usize,
-            y:      layout.location.y as usize,
-            width:  layout.size.width as usize,
-            height: layout.size.height as usize,
-            cell:   (style, c).into(),
-        }
     }
 }
