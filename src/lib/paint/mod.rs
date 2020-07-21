211a7a4cb5c284f740a38aa4234984ea0b051828
Romain TRUCHI <romain.truchi.06@gmail.com>
2020-07-21 14:08:07 +0200

    Paint

--- src/lib/paint/mod.rs
+++ src/lib/paint/mod.rs
@@ -5,13 +5,13 @@
 mod canvas;
 mod cell;
 mod layer;
+mod paint;
 mod rect;
-// mod paint;
 mod uniform;
 
 pub use canvas::*;
 pub use cell::*;
 pub use layer::*;
+pub use paint::*;
 pub use rect::*;
-// pub use paint::*;
 pub use uniform::*;
--- src/lib/paint/paint.rs
+++ src/lib/paint/paint.rs
@@ -14,6 +14,7 @@ use stretch::result::Layout;
 // end_bottom: Layer,
 // }
 
+/// A `Paint`ed `Component` (background, content)
 #[derive(Default)]
 pub struct Paint {
     background: Uniform,
@@ -22,26 +23,28 @@ pub struct Paint {
 }
 
 impl Paint {
+    /// Paint `self` above `canvas`
     pub fn above(&self, canvas: &mut Canvas) {
         canvas.above(&self.background);
         canvas.above(&self.content);
     }
 
+    /// Paint `self` below `canvas`
     pub fn below(&self, canvas: &mut Canvas) {
         canvas.below(&self.background);
         canvas.below(&self.content);
     }
 }
 
+/// Creates a `Paint`
 impl From<(Layout, ColorStyle, Content)> for Paint {
     fn from((layout, style, content): (Layout, ColorStyle, Content)) -> Self {
         let content = match content {
             Content::None => Default::default(),
-            Content::Char(c) => (layout, style, c).into(),
-            Content::String(s) => (layout, style, s).into(),
+            Content::Char(char) => (layout, style, char).into(),
+            Content::String(string) => (layout, style, string).into(),
         };
-
-        let background = (layout, style, ' ').into();
+        let background = (layout, style.into()).into();
 
         Self {
             content,
